cfd32c3aa1730ec91e086a6ce0bc0d4f28ac0e65
hundun-elon <mbuyazisiphamandlahundun@gmail.com>
2025-03-19 19:09:15 +0200

    built the template for the neural network module

---
+++ neural_network.py
@@ -0,0 +1,22 @@
+import numpy as np
+# do not use the inbuilt python libraries besides numpy.
+class Neural_Network(object):
+
+      def __init__(self):
+            pass 
+            # I should be thinking about inintializing the attributes here.
+      
+      def least_squares(self,y,t):
+            # y is the output 1d-matrix and t is the target 1d-matrix
+            pass
+
+      def sigmoid(self, z):
+            # z is the input to the sigmoid function
+
+            pass 
+
+      def forward_propagation(self):
+            pass
+
+      def back_propagation(self):
+            pass 
