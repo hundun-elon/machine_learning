bcddb4e76b224ad8e9f27baf60000796705e33f5
hundun-elon <mbuyazisiphamandlahundun@gmail.com>
2025-03-19 19:18:33 +0200

    added the logic for least_squares and sigmoid activation function

--- neural_network.py
+++ neural_network.py
@@ -3,20 +3,35 @@ import numpy as np
 class Neural_Network(object):
 
       def __init__(self):
+            # initiate the weights and biases matrices
+            
             pass 
             # I should be thinking about inintializing the attributes here.
       
       def least_squares(self,y,t):
             # y is the output 1d-matrix and t is the target 1d-matrix
-            pass
+            error = 0 
+
+            for i in range(len(y)):
+                  error+= (y[i]-t[i])**2
+            results = 1/2*error 
+
+            return results
 
       def sigmoid(self, z):
             # z is the input to the sigmoid function
-
-            pass 
+            results = 1/ (1+ np.exp(-1*z))
+             
 
       def forward_propagation(self):
             pass
 
       def back_propagation(self):
             pass 
+
+def main():
+      # actually instantiate a neural network.
+      pass
+
+if __name__ =="__main__":
+      main()
