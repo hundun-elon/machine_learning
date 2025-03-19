35310c841ddae0f5ef311aec7e6e27b0872e1d8d
hundun-elon <mbuyazisiphamandlahundun@gmail.com>
2025-03-19 19:54:53 +0200

    added the initial weights and biases for this nueral network

--- neural_network.py
+++ neural_network.py
@@ -1,13 +1,23 @@
 import numpy as np
 # do not use the inbuilt python libraries besides numpy.
-class Neural_Network(object):
-
+class Neural_Network():
       def __init__(self):
             # initiate the weights and biases matrices
             
-            pass 
+            # pass 
             # I should be thinking about inintializing the attributes here.
-      
+
+            # the weights.
+            self.weight1 = np.ones((10,5))
+            self.weight2 = np.ones((10,3))
+
+            # the biases.
+            self.biases1 = np.ones((10,1))
+            self.biases2 = np.ones((3,1))
+      def print_attr(self):
+            print(
+                 self.weight1 , self.weight2
+            )
       def least_squares(self,y,t):
             # y is the output 1d-matrix and t is the target 1d-matrix
             error = 0 
@@ -21,7 +31,9 @@ class Neural_Network(object):
       def sigmoid(self, z):
             # z is the input to the sigmoid function
             results = 1/ (1+ np.exp(-1*z))
-             
+            return results
+      
+
 
       def forward_propagation(self):
             pass
@@ -31,7 +43,10 @@ class Neural_Network(object):
 
 def main():
       # actually instantiate a neural network.
-      pass
+      # pass 
+
+      n =Neural_Network()
+      n.print_attr()
 
 if __name__ =="__main__":
       main()
